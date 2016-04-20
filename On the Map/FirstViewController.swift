6f378e6296f5b0a5a156949c4ae2414f00fda2ed
leanne63 <leanne63@users.noreply.github.com>
2016-04-20 13:28:24 -0500

    initial commit

---
+++ On the Map/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  On the Map
+//
+//  Created by leanne on 4/20/16.
+//  Copyright © 2016 leanne63. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
