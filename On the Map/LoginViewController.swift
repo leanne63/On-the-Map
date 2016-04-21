98f9be40a5b39e2bc80cb75e69c8cdfa91bcb072
leanne63 <leanne63@users.noreply.github.com>
2016-04-21 13:55:44 -0500

    stackview added and constrained

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -21,8 +21,7 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
+	
     /*
     // MARK: - Navigation
 
