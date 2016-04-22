0c29a10c12d698aa3c293b3850c4a8b2bfe3343e
leanne63 <leanne63@users.noreply.github.com>
2016-04-21 23:30:16 -0500

    added gradient handling extension

---
+++ On the Map/LoginView.swift
@@ -0,0 +1,17 @@
+//
+//  LoginView.swift
+//  On the Map
+//
+//  Created by leanne on 4/21/16.
+//  Copyright © 2016 leanne63. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+	override class func layerClass() -> AnyClass {
+		return CAGradientLayer.self
+	}
+
+}
--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -12,24 +12,13 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.layer.configureGradientBackground(UIColor.whiteColor().CGColor, UIColor.blueColor().CGColor)
-		//view.layer.configureGradientBackground(UIColor.purpleColor().CGColor, UIColor.blueColor().CGColor, UIColor.whiteColor().CGColor)
+		
+		setUpViewGradient(withColor: UIColor.orangeColor())
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-	
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+		// locking this (login view) to portrait since subviews won't all fit on smaller devices in landscape
+		return .Portrait
+	}
 
 }
