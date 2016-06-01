45c43396fa00f8965fca5b85f6d7e6021ba0ea10
leanne63 <leanne63@users.noreply.github.com>
2016-06-01 15:54:18 -0500

    moved validation functions into login model; chg'd some error wording

--- On the Map/Login.swift
+++ On the Map/Login.swift
@@ -44,15 +44,104 @@ struct Login {
 	
 	// MARK: - Functions
 	
-	func login() {
+	func loginToUdacity() {
 		// TODO: POST request to get session id
 		// https://docs.google.com/document/d/1MECZgeASBDYrbBg7RlRu9zBBLGd3_kfzsN-0FtURqn0/pub?embedded=true
 		print("IN: \(#function)")
 	}
 
-	func logout() {
+	func logoutFromUdacity() {
 		// TODO: DELETE request with session id
 		print("IN: \(#function)")
 	}
+	
+	/**
+	
+	Validate login data for minimal correctness
+	
+	- returns: Tuple containing
+	- Bool indicating whether validation was successful
+	- Failure message if validation unsuccessful, nil otherwise
+	
+	*/
+	func validateLoginData(email: String?, password: String?) -> (isSuccess: Bool, errorMsg: String?) {
+		
+		var returnBool = true
+		var failMessage: String? = nil
+		
+		// validate login email and password aren't empty
+		guard let email = email where !email.isEmpty,
+			let password = password where !password.isEmpty else {
+				returnBool = false
+				failMessage = "login email and password are both required"
+				return (returnBool, failMessage)
+		}
+		
+		// validate basic email format
+		let successData: (isSuccess: Bool, errorMsg: String?) = validateEmailAddressFormat(email)
+		if !successData.isSuccess {
+			returnBool = false
+			failMessage = successData.errorMsg
+			return (returnBool, failMessage)
+		}
+		
+		return (returnBool, failMessage)
+	}
+	
+	
+	/**
+	
+	Validate email address string for correct format
+	
+	- parameter emailAddress: an email address to validate
+	
+	- returns: Tuple containing:
+	- Bool indicating whether validation was successful
+	- Failure message if validation unsuccessful, nil otherwise
+	
+	*/
+	private func validateEmailAddressFormat(emailAddress: String) -> (Bool, String?) {
+		
+		var returnBool = true
+		var failMessage: String? = nil
+		
+		// NSRegularExpression to ensure email in at least a correct format before sending
+		// [A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}
+		// [A-Z0-9._%+-]+
+		//		matches letters A thru Z, digits 0 thru 9, dot, underscore, percent, plus, hyphen
+		//			occurring 1 or more times (+)
+		// @ matches literal "at" character
+		// [A-Z0-9.-]+
+		//		matches letters A thru Z, digits 0 thru 9, dot, hyphen occurring 1 or more times (+)
+		// \. matches literal "dot" character
+		//		(note: our version has two backslashes - the first is escaping the 2nd, real backslash)
+		// [A-Z]{2,}
+		//		matches letters A thru Z occurring 2 or more times
+		// Note: regular expression matches are case sensitive by default;
+		//	we'll use NSRegularExpressionOptions.CaseInsensitive to ignore that
+		let regexPattern = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}"
+		guard let regex = try? NSRegularExpression(pattern: regexPattern, options: .CaseInsensitive) else {
+			returnBool = false
+			failMessage = "unable to validate email address"
+			return (returnBool, failMessage)
+		}
+		
+		// email address will be processed under the hood as an NSString
+		//	if it contains UTF multi-code-unit characters, its length will differ from
+		//	Swift's characters.count value; so convert to NSString for correct length
+		let searchRange = NSMakeRange(0, (emailAddress as NSString).length)
+		
+		guard regex.numberOfMatchesInString(emailAddress, options: .WithoutAnchoringBounds, range: searchRange) > 0 else {
+			returnBool = false
+			failMessage = "email address isn't formatted correctly"
+			return (returnBool, failMessage)
+		}
+		
+		// if we're here, the email has passed all the tests
+		return(returnBool, failMessage)
+		
+	}
+	
+
 
 }
--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -43,14 +43,12 @@ class LoginViewController: UIViewController {
 	
 	@IBAction func loginClicked(sender: UIButton) {
 		
-		let loginDataValidationResult = validateLoginData()
-		print("loginDataValidationResult = \(loginDataValidationResult)")
-		
+		let loginDataValidationResult = loginModel.validateLoginData(emailField.text, password: passwordField.text)
 		
 		// get Udacity session id (not retrieving data at this time)
 		if loginDataValidationResult.isSuccess {
 			// TODO: valid data exists; now login, retrieve session id
-			loginModel.login()
+			loginModel.loginToUdacity()
 			
 			// TODO: if successful login, get user data; use key-value observing
 			// TODO: alertview for login failure - why? invalid Udacity login values or network?
@@ -75,91 +73,4 @@ class LoginViewController: UIViewController {
 	
 	// MARK: - Private Functions
 	
-	/**
-	
-	Validate login data for minimal correctness
-	
-	- returns: Tuple containing
-		- Bool indicating whether validation was successful
-		- Failure message if validation unsuccessful, nil otherwise
-	
-	*/
-	private func validateLoginData() -> (isSuccess: Bool, errorMsg: String?) {
-		
-		var returnBool = true
-		var failMessage: String? = nil
-		
-		// validate login email and password aren't empty
-		guard let email = emailField.text where !email.isEmpty,
-			  let password = passwordField.text where !password.isEmpty else {
-			returnBool = false
-			failMessage = "Login email and password are both required!"
-			return (returnBool, failMessage)
-		}
-		
-		// validate basic email format
-		let successData: (isSuccess: Bool, errorMsg: String?) = validateEmailAddressFormat(email)
-		if !successData.isSuccess {
-			returnBool = false
-			failMessage = successData.errorMsg
-			return (returnBool, failMessage)
-		}
-		
-		return (returnBool, failMessage)
-	}
-	
-	
-	/**
-	
-	Validate email address string for correct format
-	
-	- parameter emailAddress: an email address to validate
-	
-	- returns: Tuple containing:
-		- Bool indicating whether validation was successful
-		- Failure message if validation unsuccessful, nil otherwise
-	
-	*/
-	private func validateEmailAddressFormat(emailAddress: String) -> (Bool, String?) {
-		
-		var returnBool = true
-		var failMessage: String? = nil
-		
-		// NSRegularExpression to ensure email in at least a correct format before sending
-		// [A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}
-		// [A-Z0-9._%+-]+
-		//		matches letters A thru Z, digits 0 thru 9, dot, underscore, percent, plus, hyphen
-		//			occurring 1 or more times (+)
-		// @ matches literal "at" character
-		// [A-Z0-9.-]+
-		//		matches letters A thru Z, digits 0 thru 9, dot, hyphen occurring 1 or more times (+)
-		// \. matches literal "dot" character
-		//		(note: our version has two backslashes - the first is escaping the 2nd, real backslash)
-		// [A-Z]{2,}
-		//		matches letters A thru Z occurring 2 or more times
-		// Note: regular expression matches are case sensitive by default;
-		//	we'll use NSRegularExpressionOptions.CaseInsensitive to ignore that
-		let regexPattern = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}"
-		guard let regex = try? NSRegularExpression(pattern: regexPattern, options: .CaseInsensitive) else {
-			returnBool = false
-			failMessage = "Unable to validate email address!"
-			return (returnBool, failMessage)
-		}
-		
-		// email address will be processed under the hood as an NSString
-		//	if it contains UTF multi-code-unit characters, its length will differ from
-		//	Swift's characters.count value; so convert to NSString for correct length
-		let searchRange = NSMakeRange(0, (emailAddress as NSString).length)
-		
-		guard regex.numberOfMatchesInString(emailAddress, options: .WithoutAnchoringBounds, range: searchRange) > 0 else {
-			returnBool = false
-			failMessage = "Email address is invalid!"
-			return (returnBool, failMessage)
-		}
-		
-		// if we're here, the email has passed all the tests
-		return(returnBool, failMessage)
-		
-	}
-
 }
